2bd86c77df61cd965fb5144d3c8080cd975eba7a
EugeneLaving <khv.evg.l.king@gmail.com>
2025-01-21 01:46:47 +1000

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+# Задание: Grade 1. Этап 1. Задание 1
+username = 'EugeneLaving'
+title ='Первая задача'
+content = 'Выполнить задание "Grade 1. Этап 1. Задание 1"'
+status = 'Выполнено'
+created_date = '21-01-2025'
+issue_date = '23-01-2025'
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания:", created_date)
+print("Дата истечения заметки:", issue_date, end=' ')
+# Надеюсь задание понял правильно и выполнил верно!
