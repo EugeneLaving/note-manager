5bf1c6b91874759992225b8a10284d114c5d9c14
EugeneLaving <khv.evg.l.king@gmail.com>
2025-01-24 00:51:49 +1000

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,18 @@
+#Задание: grade 1 этап 1 задание 3
+username =input("Введите своё имя: ")
+title_1 =input("Введите первый заголовок: ")
+title_2 =input("Введите второй заголовок: ")
+title_3 =input("Введите третий заголовок: ")
+titles =[title_1,  title_2, title_3]
+content = input("Введите содержание заметки: ")
+created_date =input("Введите дату начала (дд.мм.гг): ")
+issue_date =input("Введите срок выполнения (дд.мм.гг): ")
+print("Заметка создана:")
+print("Имя пользователя: ", username)
+print("Дата создания: ", created_date [0:5])
+print("Выполнить до: ", issue_date [0:5])
+print("Заголовки: ", titles)
+print(title_1)
+print(title_2)
+print(title_3)
+print("Содержание: ", content, end=" ")
