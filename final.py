91015d9d5f778c092a52e363cf9dd58cb070f3a4
EugeneLaving <khv.evg.l.king@gmail.com>
2025-01-24 00:52:34 +1000

    Add files via upload

---
+++ final.py
@@ -0,0 +1,15 @@
+username =input("Введите имя пользователя: ")
+title1 =input("Введите первый заголовок: ")
+title2 =input("Введите второй заголовок: ")
+title3 =input("Введите третий заголовок: ")
+content =input("Введите содержание заметки: ")
+created_date =input("Дата создания (дд.мм.гг) ")
+issue_date =input("Выполнить до (дд.мм.гг):  ")
+note =[
+    username,
+    content,
+    created_date,
+    issue_date,
+    [title1, title2, title3]
+]
+print(note)
