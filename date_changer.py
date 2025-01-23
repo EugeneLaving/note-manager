a9f96f2b89d836ecd674830f8b6bf6e4514513c6
EugeneLaving <khv.evg.l.king@gmail.com>
2025-01-24 00:46:48 +1000

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,5 @@
-#Пока не понял, что конкретно нужно, но создал алгоритм "обрезки" дедлайна.
-created_date = "23.01.25"
 username = input("Введите имя: ")
+created_date = "23.01.25"
 issue_date = input("Введите срок окончания: ")
 print(username)
 print("Дата создания: ", created_date [0:5], "; " "Дата окончания: ", issue_date[0:5], end=" ")
