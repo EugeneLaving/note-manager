8f0693e10e1f8721ee72bd0dd7a4e39c93c44d73
EugeneLaving <khv.evg.l.king@gmail.com>
2025-01-24 01:12:15 +1000

    final.py

--- final.py
+++ final.py
@@ -3,11 +3,13 @@ title1 =input("Введите первый заголовок: ")
 title2 =input("Введите второй заголовок: ")
 title3 =input("Введите третий заголовок: ")
 content =input("Введите содержание заметки: ")
+status = input("Введите статус заметки: ")
 created_date =input("Дата создания (дд.мм.гг) ")
 issue_date =input("Выполнить до (дд.мм.гг):  ")
 note =[
     username,
     content,
+    status,
     created_date,
     issue_date,
     [title1, title2, title3]
